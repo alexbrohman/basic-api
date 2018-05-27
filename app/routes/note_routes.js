7df54fe3be181c150ac2d57ce80c226d1c2d5be8
Alex Brohman <alexbrohman9@gmail.com>
2018-05-27 15:30:45 -0400

    working basic notes api

---
+++ app/routes/note_routes.js
@@ -0,0 +1,63 @@
+ /* 
+ 
+ When the app receives a post request to the ‘/notes’ path, it will execute the code inside the callback- passing in a request object (which contains the parameters or JSON of the request) and a response object (used to reply).
+ 
+ MongoDB stores data in collections- which are exactly how they sound. In your case, you want to store your notes in a collection called — you guessed it — notes.
+ 
+ */
+
+var ObjectID = require('mongodb').ObjectID;
+module.exports = function(app, db) {
+
+   //READ NOTE 
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+
+  //CREATE NOTE
+app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  //DELETE NOTE
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+
+  //UPDATE NOTE
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  });
+};
---
+++ config/db.js
@@ -0,0 +1,6 @@
+//TODO: add to git ignore file if putting this on github
+
+module.exports = {
+    url : 'mongodb://user:password@ds235840.mlab.com:35840/note-db'
+};
+  
